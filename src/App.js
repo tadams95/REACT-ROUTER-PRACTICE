972514908b61d2cbe52a5d5581c96a68db31972a
tadams95 <tyrelleadams28@gmail.com>
2023-01-03 13:02:38 -0800

    Add "Not Found" page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -22,6 +23,11 @@ function App() {
 
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+
+        {/* wildcard path or the Not found page */}
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
